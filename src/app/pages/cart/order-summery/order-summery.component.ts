9bfa0d3b7b081fbdf94d2a9a699c91478ecd8cdc
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-04-12 01:44:26 +0530

    order summery part done

---
+++ src/app/pages/cart/order-summery/order-summery.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-summery',
+  imports: [],
+  templateUrl: './order-summery.component.html',
+  styleUrl: './order-summery.component.scss'
+})
+export class OrderSummeryComponent {
+
+}
